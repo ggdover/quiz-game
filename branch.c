8d3a12afb5febcf1dcceefe141209851b3036030
Filip Doversten <dover@Filips-MacBook-Pro.local>
2015-11-30 21:54:36 +0100

    branch.c and branch.h

---
+++ branch.c
@@ -0,0 +1,13 @@
+//
+//  branch.c
+//  textQuiz
+//
+//  Created by Filip Doversten on 30/11/15.
+//  Copyright © 2015 Filip Doversten. All rights reserved.
+//
+
+#include "branch.h"
+
+int wellHiThere() {
+    return 0;
+}
