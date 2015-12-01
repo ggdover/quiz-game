208ec91b79f2aa4c9d11da13c3dab1edc246ab07
Filip Doversten <dover@wlanguest14220.eduroam.hj.se>
2015-12-01 16:13:42 +0100

    update filip.c add filip.h

--- filip.c
+++ filip.c
@@ -7,4 +7,24 @@
 //
 
 #include <stdio.h>
+#include "filip.h"
 
+int displayQuestionWithRightAnswer(question q) {
+    
+    clearScreen();
+    
+    printf("%s\n\n", q.q);
+    
+    printf("%s\n%s\n%s\n%s", q.a0, q.a1, q.a2, q.a3);
+    
+    
+    
+    return 0;
+}
+
+void clearScreen() {
+
+    for (int i = 0; i < SCREEN_HEIGHT; i++) {
+        putchar('\n');
+    }
+}
