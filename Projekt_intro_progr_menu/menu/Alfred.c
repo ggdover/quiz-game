936a75349607c1988fc29ef7daa9fe80d75f28fc
Alpeace59 <alfred.johanssonm@hotmail.se>
2015-12-02 13:05:50 +0100

    Uppdaterad Alfred.c

--- Projekt_intro_progr_menu/menu/Alfred.c
+++ Projekt_intro_progr_menu/menu/Alfred.c
@@ -11,31 +11,25 @@ int score_system(int QCount, int difficulty);
 
 int game_menu(){
 	char input;
+	clearScreen();
 	printf("Welcome to this game of quiz!\nYou have three options:\nPlay	  Credits	Exit\n");
 	scanf("%c", &input);
 
 	switch (input){
-	case 'P':
-	case 'p':
-		printf("Play\n");		//Funktionsanrop av game
-		system("pause");
-		return 1;
+	case 'E':
+	case 'e':
+		return 0;
 		break;
 	case 'C':
 	case 'c':
-		printf("Credits\n");	//Funktionsanrop av credits
-		system("pause");
-		return 2;
+		return 1;
 		break;
-	case 'E':
-	case 'e':
-		printf("Exit\n");		//Avslutar spelet
-		system("pause");
-		return 0;
+	case 'P':
+	case 'p':
+		return 2;
 		break;
 	default:
 		printf("Wrong input try again\n");			//Vid input som inte matchar något av alternativen
-		system("pause");
 		return 3;
 		break;
 	}
